37d6ca4b45751d721970c034025f35ec12ce6d6c
hAWK_ <hawk.dvp@gmail.com>
2017-01-25 14:07:55 +0200

    Implement SetMass, ApplyForce and Update for Mover

--- vectors/mover.go
+++ vectors/mover.go
@@ -12,6 +12,7 @@ type Mover struct {
 	acceleration *vector.GVector
 	velocity     *vector.GVector
 	location     *vector.GVector
+	mass         float64
 }
 
 // NewMover creates an object of type Mover (constructor)
@@ -36,17 +37,33 @@ func NewMover(obj interface{}, location *vector.GVector) *Mover {
 	return mover
 }
 
-// ApplyForce ...
+// SetMass assigns the mass argument to the object's mass
+func (m *Mover) SetMass(mass float64) {
+	m.mass = mass
+}
+
+// ApplyForce adds the force vector the object's acceleration vector
 func (m *Mover) ApplyForce(force *vector.Vector) {
-	// todo
+	// Newton's 2nd law: Acceleration = Sum of all forces / Mass
+	fCopy := force.Copy()
+	// Apply the full law only if the mass has been set
+	if mass > 0 {
+		fCopy.Divide(mass)
+	}
+	m.acceleration.Add(fCopy)
 }
 
-// Update ...
+// Update modifies the object's location depending on the applied forces;
+// Should be called on every rendering iteration
 func (m *Mover) Update() {
-	// todo
+	// We keep the velocity only for correctness based on physics laws
+	m.velocity.Add(m.acceleration)
+	m.location.Add(m.velocity)
+	// Clear the acceleration
+	m.acceleration.Multiply(0)
 }
 
-// PixelLoc returns the rounded values of vector's X and Y
+// PixelLoc returns the rounded values of location's X and Y which are ready for rendering
 func (m *Mover) PixelLoc() (int, int) {
 	return round(m.location.x), round(m.location.y)
 }
