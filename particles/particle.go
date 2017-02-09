6088fa64b930fca8f051516f8d5bd5ddc91d3cd8
hAWK_ <hawk.dvp@gmail.com>
2017-02-09 17:27:09 +0200

    Add push force to a Particle

--- particles/particle.go
+++ particles/particle.go
@@ -1,14 +1,21 @@
 package particles
 
-import "github.com/hAWKdv/go-gravity/vectors/vectors"
+import (
+	"math/rand"
+
+	"github.com/hAWKdv/go-gravity/vectors/forces"
+	"github.com/hAWKdv/go-gravity/vectors/vectors"
+)
 
 const particleLifespan = 100
+const defaultPushDuration = 60 // Frames
 
 var particleCt int
 
 // Particle keeps vectors.Mover and adds a lifespan property
 type Particle struct {
 	mover    *vectors.Mover
+	push     *forces.Push
 	lifespan int
 	id       int
 }
@@ -16,5 +23,11 @@ type Particle struct {
 // NewParticle creates an object of type Particle (constructor)
 func NewParticle(mover *vectors.Mover) *Particle {
 	particleCt++
-	return &Particle{mover, particleLifespan, particleCt}
+	pushForce := getRandomPushDirection()
+	return &Particle{mover, pushForce, particleLifespan, particleCt}
+}
+
+func getRandomPushDirection() *forces.Push {
+	dir := float64(rand.Intn(359))
+	return forces.CreatePush(dir, 0.5, defaultPushDuration)
 }
