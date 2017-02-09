c17e7b8cfc8031bf09f99833202c59048fab8d17
hAWK_ <hawk.dvp@gmail.com>
2017-02-09 15:06:10 +0200

    Add UpdateSystemSdl2()

--- particles/particle_system_ext_sdl2.go
+++ particles/particle_system_ext_sdl2.go
@@ -1,5 +1,8 @@
 package particles
 
+// UpdateSystemSdl2 execures UpdateSdl2 on every mover/particle
 func (ps *ParticleSystem) UpdateSystemSdl2() {
-
+	ps.UpdateSystem(func(p *Particle) {
+		p.mover.UpdateSdl2()
+	})
 }
