dc53477e5907c1c2d66aa823bb655eba0eebd01c
hAWK_ <hawk.dvp@gmail.com>
2017-02-18 19:20:36 +0200

    Fix random function in utils

--- vectors/utils/util.go
+++ vectors/utils/util.go
@@ -19,11 +19,8 @@ func DegToRad(deg float64) float64 {
 
 // Round performs rounding of a float type to the nearest int
 func Round(n float64) int {
-	var num float64
-	if n < 0 {
-		num = math.Ceil(n - 0.5)
+	if n > 0 {
+		return int(math.Floor(n + 0.5))
 	}
-	num = math.Floor(n + 0.5)
-
-	return int(num)
+	return int(math.Ceil(n - 0.5))
 }
